ebd9a2da0349f1c7e74c3b3a1c4ce7c63a53d5f3
aishazaki <aishazaki3@gmail.com>
2021-05-02 15:23:12 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+/* import 'bootstrap/dist/css/bootstrap.min.css'; */
+import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+import './css/style.css';
+import 'bootstrap/dist/js/bootstrap.min.js'; // 'bootstrap'اختصار
+import 'jquery/dist/jquery.min';
+import 'popper.js/dist/popper.min';
---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+var path = require("path")
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var MiniCssExtractPlugin = require("mini-css-extract-plugin")
+var OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin")
+/*zero configuration 'default' */
+module.exports = {
+    /*module.exports بمجرد تشغيل  ويب باك من خلال الامر npm run build 
+    webpack.cliقم بتصدير الاعدادات في الملف ليتم  تنفيذها وقراءاتها من قبل */
+
+    entry: {
+        app: "./src/index.js"
+    },
+
+    output: {
+        path: path.join(__dirname, "/dist"),
+        publicPath: "",
+        filename: "main.js",
+    },
+    mode: "development",
+
+    devServer: {
+        contentBase: path.join(__dirname, "/dist"),/*المسار الذي سوف تتعامل معه */
+        port: 1239,
+        writeToDisk: true,/*ليتم نسخ مجلد dist
+         لهنا بدلا من وجودة بالذكراة كماهو محدد افتراضيا */
+        open: true, /*فتح الصفحة اوتماتيكيا دون نسخ الرابط  للمتصفح */
+    },
+    module: {
+        /*لكل محمل قاعدة نكتبها داخل الخاصية rules
+        ليصبح عددها بعدد المحملات التي قمنا بتنزيلها 
+        تكتب قواعد المحملات داخل [] */
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            minimize: true,/*تفعيل ضغط الملف */
+                        }
+                    }
+                ]
+
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../'
+                        }
+                    },
+                    'css-loader',
+
+                ]
+            },
+            {
+                test: /\.(png|svg|jpe?g|gif)$/,
+                use:[
+                    {
+                        loader: "file-loader",
+                        options:{
+                            name: '[name].[ext]',
+                            outputPath: "images",
+                        }
+                    }
+                ]
+            }
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html", /*اسم الملف الذي ستنتجه ويب باك  من عمليةالبناء */
+            template: "./src/index.html",/*مسارالملف الاصلي  */
+        }),
+        new MiniCssExtractPlugin({ filename: "css/style.css" }),
+
+        new OptimizeCssAssetsPlugin({}),
+    ],
+
+};
